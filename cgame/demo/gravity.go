7adb5c380bcd7d156edb65ea466d897ea26c293f
JF Technology <57098161+jf-tech@users.noreply.github.com>
2022-02-25 15:36:13 +1300

    gravity demo now always bounce + v/h bricks

--- cgame/demo/gravity.go
+++ cgame/demo/gravity.go
@@ -15,10 +15,10 @@ import (
 )
 
 var gravityString = []string{
-	"0 char/s^2",
-	"10 char/s^2",
-	"20 char/s^2",
-	"40 char/s^2",
+	"  0 char/s^2",
+	" 10 char/s^2",
+	" 20 char/s^2",
+	" 40 char/s^2",
 	"100 char/s^2",
 	"150 char/s^2",
 }
@@ -31,8 +31,6 @@ var gravityVals = []cgame.CharPerSecSec{
 	150,
 }
 var curGravityIdx = 2
-
-var bounce = true
 
 func main() {
 	g, err := cgame.Init(cterm.TCell)
@@ -52,7 +50,12 @@ func main() {
 		Items: gravityString,
 		OnSelect: func(idx int, selected string) {
 			curGravityIdx = idx
-			g.SpriteMgr.DeleteAll()
+			ss := g.SpriteMgr.Sprites()
+			for _, s := range ss {
+				if s.Name() == particleName {
+					g.SpriteMgr.DeleteSprite(s)
+				}
+			}
 		},
 	})
 	curGravityIdx = 3
@@ -67,7 +70,7 @@ func main() {
 	demoWinR := cwin.Rect{X: 0, Y: 0, W: sysWinR.W - statsWinR.W, H: sysWinR.H}
 	demoWin := g.WinSys.CreateWin(nil, cwin.WinCfg{
 		R:    demoWinR,
-		Name: "Demo - ↑↓ to change gravity; 'b' to turn bounce on/off; space to pause/resume. Any other key to exit."})
+		Name: "Demo - ↑↓ to change gravity; space to pause/resume. Any other key to exit."})
 
 	g.WinSys.Update() // nothing shows onto screen unless Update() is called.
 	g.Resume()        // game (master clock) is always paused right after init.
@@ -82,7 +85,35 @@ type spriteParticle struct {
 	demoWinR cwin.Rect
 }
 
+func (s *spriteParticle) CollisionNotify(
+	_ bool, collidedWith []cgame.Sprite) cgame.CollisionResponseType {
+	_, vy := s.Animators()[0].(*cgame.AnimatorWaypoint).
+		Cfg().Waypoints.(*cgame.WaypointProviderAcceleration).CurSpeed()
+
+	brick := collidedWith[0]
+	brickR := brick.Rect()
+	sR := s.Rect()
+
+	if brick.Name() == hBrickName {
+		if sR.Y < brickR.Y || sR.Y > brickR.Y {
+			s.createGravityAnimator(s.vx, -vy)
+		} else {
+			s.vx = -s.vx
+			s.createGravityAnimator(s.vx, vy)
+		}
+	} else {
+		if sR.X < brickR.X || sR.X > brickR.X {
+			s.vx = -s.vx
+			s.createGravityAnimator(s.vx, vy)
+		} else {
+			s.createGravityAnimator(s.vx, -vy)
+		}
+	}
+	return cgame.CollisionResponseAbandon
+}
+
 func (s *spriteParticle) createGravityAnimator(vx, vy cgame.CharPerSec) {
+	s.DeleteAnimator(s.Animators()...)
 	s.AddAnimator(cgame.NewAnimatorWaypoint(s, cgame.AnimatorWaypointCfg{
 		Waypoints: cgame.NewWaypointProviderAcceleration(cgame.WaypointProviderAccelerationCfg{
 			Clock:      s.Game().MasterClock,
@@ -98,11 +129,8 @@ func (s *spriteParticle) createGravityAnimator(vx, vy cgame.CharPerSec) {
 			// outside the demoWin X range; for y, we do hope to see those going up will
 			// eventually (and hopefully :) coming down. Thus, we need to turn off the
 			// automatic bounds check.
-			InBoundsCheckType: cgame.InBoundsCheckNone,
-			// Turn off collision check, although strictly speaking this is not necessary
-			// since we didn't register anything in the CollidableRegistry so nothing
-			// will collide with each other anyway.
-			CollisionDetectionType: cgame.CollisionDetectionOff,
+			InBoundsCheckType:     cgame.InBoundsCheckNone,
+			KeepAliveWhenFinished: true,
 			AfterUpdate: func() {
 				if s.IsDestroyed() {
 					return
@@ -117,10 +145,8 @@ func (s *spriteParticle) createGravityAnimator(vx, vy cgame.CharPerSec) {
 					return
 				}
 				_, vy := wp.CurSpeed()
-				if bounce &&
-					s.Rect().X >= 0 && s.Rect().X < s.demoWinR.W &&
+				if s.Rect().X >= 0 && s.Rect().X < s.demoWinR.W &&
 					s.Rect().Y >= s.demoWinR.H && vy > 0 {
-					s.DeleteAnimator(s.Animators()[0])
 					s.Update(cgame.UpdateArg{
 						DXY: &cwin.Point{Y: s.demoWinR.H - s.Rect().Y - 1},
 						IBC: cgame.InBoundsCheckNone,
@@ -140,9 +166,15 @@ func (s *spriteParticle) createGravityAnimator(vx, vy cgame.CharPerSec) {
 }
 
 var (
-	particleImg         = "⬤"
+	particleImg         = "●"
 	particleName        = "particle"
 	particleFrameNoAttr = cgame.FrameFromString(particleImg, cwin.Attr{})
+
+	hBrickFrame = cgame.FrameFromString("##########", cwin.Attr{Fg: cterm.ColorDarkGray})
+	hBrickName  = "h_brick"
+
+	vBrickFrame = cgame.FrameFromString("#\n#\n#\n#\n#", cwin.Attr{Fg: cterm.ColorDarkGray})
+	vBrickName  = "v_brick"
 )
 
 func genParticleColor() cterm.Attribute {
@@ -161,11 +193,37 @@ func genParticleXSpeed() cgame.CharPerSec {
 }
 
 func genParticleYSpeed() cgame.CharPerSec {
-	return cgame.CharPerSec(rand.Int()%73 - 80) // [-80,-8]
+	return cgame.CharPerSec(rand.Int()%53 - 60) // [-60,-8]
 }
 
 func doDemo(g *cgame.Game, demoWin, statsWin cwin.Win) {
+	g.SpriteMgr.CollidableRegistry().RegisterBulk(particleName, []string{hBrickName, vBrickName})
 	r := demoWin.ClientRect().ToOrigin()
+
+	hBrickW := cgame.FrameRect(hBrickFrame).W
+	hBrickCol := 3
+	hBrickGap := (r.W - hBrickCol*hBrickW) / (hBrickCol + 1)
+	for i := 0; i < hBrickCol; i++ {
+		g.SpriteMgr.AddSprite(cgame.NewSpriteBase(g, demoWin, hBrickName, hBrickFrame,
+			hBrickGap+i*(hBrickGap+hBrickW),
+			(r.H-cgame.FrameRect(hBrickFrame).H)/2))
+		g.SpriteMgr.AddSprite(cgame.NewSpriteBase(g, demoWin, hBrickName, hBrickFrame,
+			hBrickGap+i*(hBrickGap+hBrickW),
+			(r.H-cgame.FrameRect(hBrickFrame).H)*3/4))
+	}
+
+	vBrickH := cgame.FrameRect(vBrickFrame).H
+	vBrickRow := 3
+	vBrickGap := (r.H - vBrickRow*vBrickH) / (vBrickRow + 1)
+	for i := 0; i < vBrickRow; i++ {
+		g.SpriteMgr.AddSprite(cgame.NewSpriteBase(g, demoWin, vBrickName, vBrickFrame,
+			r.W/20,
+			vBrickGap+i*(vBrickGap+vBrickH)))
+		g.SpriteMgr.AddSprite(cgame.NewSpriteBase(g, demoWin, vBrickName, vBrickFrame,
+			r.W*19/20,
+			vBrickGap+i*(vBrickGap+vBrickH)))
+	}
+
 	createParticle := func(x, y int, vx, vy cgame.CharPerSec, color cterm.Attribute) {
 		attr := cwin.Attr{Fg: color}
 		p := &spriteParticle{
@@ -184,7 +242,6 @@ func doDemo(g *cgame.Game, demoWin, statsWin cwin.Win) {
 		var sb strings.Builder
 		sb.WriteString(fmt.Sprint("Stats:\n"))
 		sb.WriteString(fmt.Sprintf("- Time: %s\n", g.MasterClock.Now().Round(time.Millisecond)))
-		sb.WriteString(fmt.Sprintf("- Bounce: %t\n", bounce))
 		sb.WriteString(fmt.Sprintf("- Particles: %d\n", len(g.SpriteMgr.Sprites())))
 		sb.WriteString(fmt.Sprintf("- FPS: %d\n", g.WinSys.FPS()))
 		sb.WriteString(fmt.Sprintf("- Mem: %s\n", cwin.ByteSizeStr(g.HeapUsageInBytes())))
@@ -193,6 +250,9 @@ func doDemo(g *cgame.Game, demoWin, statsWin cwin.Win) {
 		sb.WriteString(fmt.Sprint("\n"))
 		sb.WriteString(fmt.Sprint("Particles:\n"))
 		for _, s := range g.SpriteMgr.Sprites() {
+			if s.Name() != particleName {
+				continue
+			}
 			sp := s.(*spriteParticle)
 			wpAcc := sp.Animators()[0].(*cgame.AnimatorWaypoint).Cfg().Waypoints.(*cgame.WaypointProviderAcceleration)
 			vx, vy := wpAcc.CurSpeed()
@@ -203,7 +263,7 @@ func doDemo(g *cgame.Game, demoWin, statsWin cwin.Win) {
 		statsWin.SetText(sb.String())
 	}
 
-	prob := cutil.NewPeriodicProbabilityChecker("20%", 100*time.Millisecond)
+	prob := cutil.NewPeriodicProbabilityChecker("50%", 100*time.Millisecond)
 	prob.Reset(g.MasterClock)
 	g.Run(nil, cwin.Keys(' '), func(ev cterm.Event) cwin.EventResponse {
 		showStats()
@@ -216,11 +276,6 @@ func doDemo(g *cgame.Game, demoWin, statsWin cwin.Win) {
 		}
 		if ev.Type != cterm.EventKey {
 			return cwin.EventNotHandled
-		}
-		if ev.Ch == 'b' {
-			bounce = !bounce
-			g.SpriteMgr.DeleteAll()
-			return cwin.EventHandled
 		}
 		return cwin.EventLoopStop
 	})
